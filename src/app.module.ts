a9fd0eba3d9cdcdefc9484b24ba3ecefce9145e3
Administrator <gitlab_admin_b5a6fd@example.com>
2025-11-16 15:11:31 +0300

    chore(config): add mongoose/event emitter/bootstrap validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    EventEmitterModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/truden'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
